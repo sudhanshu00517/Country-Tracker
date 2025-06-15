6b92fb3a1b112d8eb30a4aaabedce49ba614580d
sudhanshu00517 <sudhanshu00517@gmail.com>
2025-06-16 00:07:52 +0530

    changes

--- index.js
+++ index.js
@@ -19,10 +19,7 @@ app.use(express.static("public"));
 
 let currentUserId = 1;
 
-let users = [
-  { id: 1, name: "Angela", color: "teal" },
-  { id: 2, name: "Jack", color: "powderblue" },
-];
+let users = [];
 
 async function checkVisisted() {
   const result = await db.query(
